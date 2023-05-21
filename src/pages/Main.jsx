3558ab38e378c6932f19596b5ff45de297076637
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-05-21 22:11:32 +0200

    added MainContext

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,24 +1,44 @@
 import React, { useEffect, useState } from 'react'
 import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from '../firebase';
+import { auth, db } from '../firebase';
 import MainNav from '../components/MainNav';
 import DirectMessages from '../components/DirectMessages';
 import Server from '../components/Server';
+import defaultpfp from '../media/img/defaultpfp.png'
+import { MainContext } from '../contexts/MainContext'
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { collection } from 'firebase/firestore';
 
 export default function Main() {
   const [activeDiv, setActiveDiv] = useState('dm')
+  const [myPfp, setMypfp] = useState('')
+  const [myName, setMyName] = useState('')
+  const [allUsers] = useCollectionData(collection(db, 'users'), {
+    snapshotListenOptions: { includeMetadataChanges: true },
+    transform: (doc) => ({ id: doc.id }),
+  });
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (!user) {
         navigate('/')
+      }else{
+        setTimeout(() => {
+          setMyName(user.displayName)
+
+          if(user.photoURL === null){
+            setMypfp(defaultpfp)
+          }else{
+            setMypfp(user.photoURL)
+          }
+        }, [1000])
       }
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
-    <>
+    <MainContext.Provider value={{ myName, myPfp, allUsers }}>
       <div className="w-screen h-screen grid grid-cols-main overflow-hidden">
         <MainNav 
           setActiveDiv={setActiveDiv}
@@ -31,6 +51,6 @@ export default function Main() {
           <Server />
         }
       </div>
-    </>
+    </MainContext.Provider>
   )
 }
