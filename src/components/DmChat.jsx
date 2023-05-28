b45c74ba506db99c9d7137b0c3210d3e4382533f
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-05-28 13:25:56 +0200

    added MessageDate component

--- src/components/DmChat.jsx
+++ src/components/DmChat.jsx
@@ -5,6 +5,7 @@ import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
 import { db } from '../firebase'
 import defaultpfp from '../media/img/defaultpfp.png'
 import MessageInput from './MessageInput'
+import MessageDate from './MessageDate'
 
 export default function DmChat({ activeConversationName }) {
   const [messages, setMessages] = useState([])
@@ -80,10 +81,9 @@ export default function DmChat({ activeConversationName }) {
             return(
             <div>
                 {newDay &&
-                  <div className='flex justify-center text-xs font-semibold text-gray-14 relative px-4 mt-[17px]'>
-                    <p className='z-10 px-2 py-px bg-main-gray'>{newDayDateFormat}</p>
-                    <div className='w-[calc(100%-32px)] bg-gray-10 h-px absolute top-2'></div>
-                  </div> 
+                  <MessageDate 
+                    newDayDateFormat={newDayDateFormat}
+                  />
                 }
 
                 {isSameSender && !newDay ? 
---
+++ src/components/MessageDate.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function MessageDate({ newDayDateFormat }) {
+  return (
+    <div className='flex justify-center text-xs font-semibold text-gray-14 relative px-4 mt-[17px]'>
+        <p className='z-10 px-2 py-px bg-main-gray'>{newDayDateFormat}</p>
+        <div className='w-[calc(100%-32px)] bg-gray-10 h-px absolute top-2'></div>
+    </div> 
+  )
+}
