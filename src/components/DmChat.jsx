5f27a95ad44460192660488362cc953a2e44a947
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-05-28 17:34:57 +0200

    added styling to div that wraps whole component

--- src/components/DmChat.jsx
+++ src/components/DmChat.jsx
@@ -35,80 +35,78 @@ export default function DmChat({ activeConversationName }) {
   }, [activeConversationName])
 
   return (
-    <>
-      <div>
-        <DmChatNav 
-            activeConversationName={activeConversationName}
-        />
-        <div className='bg-main-gray h-[calc(100%-117px)] pb-5 overflow-y-scroll chatbar text-overlay-text'>
-          {messages.map(message => {
-            let isSameSender, firstMessageDate;
-
-            let newDay = false
-
-            let thisIndex = messages.indexOf(message)
-
-            let currentDate = new Date()
-
-            let messageDate = new Date(message.createdAt.seconds * 1000);
-
-            let formattedTime = messageDate.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
-
-            let dayDiff = currentDate.getDate() - messageDate.getDate()
-
-            let newDayDateFormat = messageDate.toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric'})
-
-            if(thisIndex === 0){
-              isSameSender = false;
-              newDay = true
-            }else{
-              isSameSender = (message.displayName === messages[thisIndex - 1].displayName)
-
-              if(new Date(message.createdAt.seconds * 1000).getDay() !== new Date(messages[thisIndex - 1].createdAt.seconds * 1000).getDay()){
-                  newDay = true
-              }
-            }
-
-            if(dayDiff === 0){
-              firstMessageDate = 'Today at ' + formattedTime
-            }else if(dayDiff === 1){
-              firstMessageDate = 'Yesterday at ' + formattedTime
-            }else if(dayDiff > 1){
-              firstMessageDate = messageDate.toLocaleDateString("en-US") + ' ' + formattedTime
-            }else{
-              firstMessageDate = formattedTime
-            }
-            
-            return(
-            <div key={crypto.randomUUID()}>
-                {newDay &&
-                  <MessageDate 
-                    newDayDateFormat={newDayDateFormat}
-                  />
-                }
-
-                {isSameSender && !newDay ? 
-                  <SameSenderMessage 
-                    formattedTime={formattedTime}
-                    msg={message.text}
-                  />
-                :
-                  <Message 
-                    sender={message.displayName}
-                    date={firstMessageDate}
-                    msg={message.text}
-                  />
-                }
-            </div>
-        )})}
-        <span ref={dummy}></span>
-        </div>
-        <MessageInput 
-          myName={myName}
+    <div className='max-h-screen min-h-screen relative bg-main-gray'>
+      <DmChatNav 
           activeConversationName={activeConversationName}
-          dummy={dummy}
-        />
+      />
+      <div className='bg-main-gray h-[calc(100%-117px)] pb-5 overflow-y-scroll chatbar text-overlay-text'>
+        {messages.map(message => {
+          let isSameSender, firstMessageDate;
+
+          let newDay = false
+
+          let thisIndex = messages.indexOf(message)
+
+          let currentDate = new Date()
+
+          let messageDate = new Date(message.createdAt.seconds * 1000);
+
+          let formattedTime = messageDate.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+
+          let dayDiff = currentDate.getDate() - messageDate.getDate()
+
+          let newDayDateFormat = messageDate.toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric'})
+
+          if(thisIndex === 0){
+            isSameSender = false;
+            newDay = true
+          }else{
+            isSameSender = (message.displayName === messages[thisIndex - 1].displayName)
+
+            if(new Date(message.createdAt.seconds * 1000).getDay() !== new Date(messages[thisIndex - 1].createdAt.seconds * 1000).getDay()){
+                newDay = true
+            }
+          }
+
+          if(dayDiff === 0){
+            firstMessageDate = 'Today at ' + formattedTime
+          }else if(dayDiff === 1){
+            firstMessageDate = 'Yesterday at ' + formattedTime
+          }else if(dayDiff > 1){
+            firstMessageDate = messageDate.toLocaleDateString("en-US") + ' ' + formattedTime
+          }else{
+            firstMessageDate = formattedTime
+          }
+          
+          return(
+          <div key={crypto.randomUUID()}>
+              {newDay &&
+                <MessageDate 
+                  newDayDateFormat={newDayDateFormat}
+                />
+              }
+
+              {isSameSender && !newDay ? 
+                <SameSenderMessage 
+                  formattedTime={formattedTime}
+                  msg={message.text}
+                />
+              :
+                <Message 
+                  sender={message.displayName}
+                  date={firstMessageDate}
+                  msg={message.text}
+                />
+              }
+          </div>
+      )})}
+      <span ref={dummy}></span>
       </div>
-    </>
+      <MessageInput 
+        myName={myName}
+        activeConversationName={activeConversationName}
+        dummy={dummy}
+      />
+    </div>
   )
 }
