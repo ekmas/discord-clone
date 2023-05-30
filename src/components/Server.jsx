69efc4b16feecc53a72e38e736e805ae5ffae8bc
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-05-30 21:16:37 +0200

    added ServerSidebar and ServerChannelButton

--- src/components/Server.jsx
+++ src/components/Server.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ServerSidebar from './ServerSidebar'
 
 export default function Server() {
+  const [activeChannelName, setActiveChannelName] = useState('channel-1')
+
   return (
-    <div>Server</div>
+    <div className='grid grid-cols-dm m850:grid-cols-dm-responsive m500:grid-cols-1'>
+      <ServerSidebar 
+        activeChannelName={activeChannelName}
+        setActiveChannelName={setActiveChannelName}
+      />
+
+      
+    </div>
   )
 }
---
+++ src/components/ServerSidebar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import User from './User'
+import ServerChannelButton from './ServerChannelButton'
+
+export default function ServerLeftSidebar({ activeChannelName, setActiveChannelName }) {
+  const channels = ['channel-1', 'channel-2', 'channel-3']
+
+  return (
+    <div className='bg-gray-4 relative max-h-screen m550:hidden'>
+        <div className='flex items-center font-semibold text-base px-4 py-3 text-white border-b border-black h-[49px]'>
+            <p>Server</p>
+        </div>
+
+        <div className='flex flex-col h-[calc(100vh-49px)]'>
+            <div className='pt-[19px] flex-1 overflow-y-scroll dropdownscrollbar'>
+                <div className='flex items-center mb-[5px] ml-4'>
+                  <p className='text-gray-3 text-xs font-semibold tracking-[0.02em]'>TEXT CHANNELS</p>
+                </div>
+
+                {channels.map(channel => {
+                  return(
+                    <ServerChannelButton 
+                      channel={channel}
+                      activeChannelName={activeChannelName}
+                      setActiveChannelName={setActiveChannelName}
+                    />
+                  )
+                })}
+            </div>
+
+            <User/>
+        </div>
+    </div>
+  )
+}
