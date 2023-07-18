05ef7fb68284ff1b682643eb901ea126e1c689a0
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-07-19 01:50:08 +0200

    added full-page class

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -39,7 +39,7 @@ export default function Main() {
 
   return (
     <MainContext.Provider value={{ myName, allUsers }}>
-      <div className="w-screen h-screen grid grid-cols-main overflow-hidden">
+      <div className="w-screen full-page grid grid-cols-main overflow-hidden">
         <MainNav setActiveDiv={setActiveDiv} activeDiv={activeDiv} />
 
         {activeDiv === 'dm' ? <DirectMessages /> : <Server />}
@@ -48,8 +48,8 @@ export default function Main() {
         style={{ transition: 'all ease 0.5s' }}
         className={
           loading
-            ? 'select-none w-screen h-screen z-50 opacity-100 visible fixed flex flex-col items-center justify-center top-0 left-0 bg-black-3'
-            : 'select-none w-screen h-screen invisible z-50 flex items-center justify-center flex-col opacity-0 fixed top-0 left-0 bg-black-3'
+            ? 'select-none w-screen full-page z-50 opacity-100 visible fixed flex flex-col items-center justify-center top-0 left-0 bg-black-3'
+            : 'select-none w-screen full-page invisible z-50 flex items-center justify-center flex-col opacity-0 fixed top-0 left-0 bg-black-3'
         }
       >
         <img src={discordlogo} width={72} alt="" />
